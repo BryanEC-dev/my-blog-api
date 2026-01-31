a234cb14c1bc22edc43416ba1fceefc20921e201
bryanEC <bryan_desarrollo_ec@outlook.com>
2026-01-31 16:43:22 -0500

    inicio del curso pratico de nest

---
+++ src/users/users.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import e from 'express';
+
+@Controller('users')
+export class UsersController {
+  private users = [
+    { id: 1, name: 'Alice', email: 'alice@example.com' },
+    { id: 2, name: 'Bob', email: 'bob@example.com' },
+    { id: 3, name: 'Charlie', email: 'charlie@example.com' },
+  ];
+
+  @Get() getAllUsers() {
+    return this.users;
+  }
+
+  @Get(':id') getUserById(@Param('id') id: number) {
+    // Implementation to get user by ID
+    return this.users.find((user) => user.id == id);
+  }
+}
